355c6326727faaa5bfa40a9345fa921e5b038fe2
arturx <arturxdroid@gmail.com>
2019-11-23 01:20:45 +0300

    Added bottom dialog with recognition result

--- app/src/main/java/ai/arturxdroid/mushroomtify/RecognitionActivity.kt
+++ app/src/main/java/ai/arturxdroid/mushroomtify/RecognitionActivity.kt
@@ -4,14 +4,15 @@ import android.graphics.Bitmap
 import android.net.Uri
 import android.os.Bundle
 import android.provider.MediaStore
-import android.util.Log
+import android.view.LayoutInflater
 import android.view.View
+import android.widget.LinearLayout
 import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.Observer
 import com.google.android.material.bottomsheet.BottomSheetDialog
 import kotlinx.android.synthetic.main.activity_recognition.*
-import kotlinx.android.synthetic.main.mushroom_item.*
+import kotlinx.android.synthetic.main.mushroom_item.view.*
 import kotlinx.coroutines.*
 
 class RecognitionActivity : AppCompatActivity() {
@@ -19,7 +20,7 @@ class RecognitionActivity : AppCompatActivity() {
     private lateinit var classifier: Classifier
     private val job = Job()
     private val coroutineScope = CoroutineScope(job)
-    private lateinit var dialog:BottomSheetDialog
+    private lateinit var dialog: BottomSheetDialog
     private val currentRecognizedText: MutableLiveData<String> by lazy {
         MutableLiveData<String>()
     }
@@ -41,38 +42,78 @@ class RecognitionActivity : AppCompatActivity() {
             MediaStore.Images.Media.getBitmap(this.contentResolver, Uri.parse(uriString))
         mushroom_recognition_image_view.setImageBitmap(imageBitmap)
         initClassification(imageBitmap)
-        val textObserver = Observer<String> { newName ->
-//            recognized_name_text_view.text = newName
-        }
-        currentRecognizedText.observe(this, textObserver)
         dialog = BottomSheetDialog(this)
-        dialog.setContentView(R.layout.mushroom_item)
-//        dialog.show()
     }
 
     private fun initClassification(imageBitmap: Bitmap) {
-        var text: List<String>
+        var indexes: List<Int>
         progress_bar.show()
         progress_bar.animate()
         coroutineScope.launch {
-            text = predict(imageBitmap)
+            indexes = predict(imageBitmap)
             delay(4000)
             withContext(Dispatchers.Main) {
-                progress_layout.visibility = View.GONE
                 progress_bar.hide()
-                Log.i("FULL_INFO",text[0])
-                dialog.mushroom_name_text_view.text = text[1]
-                dialog.mushroom_edible_text_view.text = "edible"
-                dialog.mushroom_edible_text_view.setTextColor(resources.getColor(R.color.colorPrimaryDark))
-                dialog.show()
+                progress_layout.visibility = View.INVISIBLE
+                show_results_button.isEnabled = true
+                prepareDialog(indexes, dialog)
             }
         }
     }
 
-    private fun predict(imageBitmap: Bitmap): List<String> {
+    private fun prepareDialog(indexes: List<Int>, dialog: BottomSheetDialog) {
+        val dialogView = LayoutInflater.from(this).inflate(
+            R.layout.recognized_dialog_layout,
+            null
+        ) as LinearLayout
+
+        val firstMushroomView = LayoutInflater.from(this).inflate(R.layout.mushroom_item, null)
+        val secondMushroomView = LayoutInflater.from(this).inflate(R.layout.mushroom_item, null)
+        val thirdMushroomView = LayoutInflater.from(this).inflate(R.layout.mushroom_item, null)
+
+        val localNames = resources.getStringArray(R.array.mushroom_names_array)
+        val edibility = resources.getStringArray(R.array.mushroom_edibility_array)
+
+        val itemViews = arrayListOf(firstMushroomView, secondMushroomView, thirdMushroomView)
+        for (viewIndex in 0..2) {
+            val view = itemViews[viewIndex]
+            view.mushroom_name_text_view?.text = Constants.LABELS_LIST[indexes[viewIndex]]
+            view.mushroom_edibility?.text = edibility[indexes[viewIndex]]
+            view.mushroom_local_name?.text = localNames[indexes[viewIndex]]
+            view.mushroom_edibility?.setTextColor(getEdibilityColor(view.mushroom_edibility.text.toString()))
+            view.mushroom_icon?.setImageResource(getMushroomIcon(view.mushroom_edibility.text.toString()))
+        }
+
+        dialogView.addView(firstMushroomView, 1)
+        dialogView.addView(secondMushroomView, 3)
+        dialogView.addView(thirdMushroomView, 4)
+
+        show_results_button.setOnClickListener { dialog.show() }
+
+        dialog.setContentView(dialogView)
+        dialog.show()
+    }
+
+    private fun getMushroomIcon(type: String): Int {
+        return when (type.toLowerCase()) {
+            "edible" -> R.drawable.good_mushroom
+            else -> R.drawable.bad_mushroom
+        }
+
+    }
+
+    private fun getEdibilityColor(edibility: String): Int {
+        return when (edibility.toLowerCase()) {
+            "edible" -> getColor(android.R.color.holo_green_dark)
+            "inedible" -> getColor(androidx.appcompat.R.color.material_grey_600)
+            else -> getColor(android.R.color.holo_red_dark)
+        }
+    }
+
+    private fun predict(imageBitmap: Bitmap): List<Int> {
         classifier = Classifier(Utils.assetFilePath(this, "bkp.pt"))
         val text =
-            classifier.predict(imageBitmap, true) + "\n\n" + classifier.predict(imageBitmap)
+            classifier.predict(imageBitmap, true)
         return text
     }
 }
